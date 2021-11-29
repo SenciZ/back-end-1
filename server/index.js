18648dccc99bd4d034c3a94640f18f112fe46612
Senad Zenkovic <zenkovic88@gmail.com>
2021-11-29 13:57:46 -0700

    complete lab

--- server/index.js
+++ server/index.js
@@ -5,7 +5,15 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get('/api/users', (req, res)=>{
+    let friends = ['Nitin','Eric','Jeddy','Cameron','Riley'];
+    res.status(200).send(friends)
+})
 
-
+app.get('/weather/:temperature', (req, res) =>{
+    const {temp} = req.params
+    const phrase = `<h3> It was ${temp} today</h3>`;
+    res.status(200).send(phrase)
+})
 
 app.listen(4000, ()=>{console.log("Server running on port 4000")})
